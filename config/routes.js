d42d3465920cf963b455c581804ef95973a58032
Mike Klophaus <mmklophaus@gmail.com>
2016-01-20 11:25:00 -0800

    modified routes for passport/google OAuth

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    passport    = require('passport'),
     router  = new express.Router();
 
 // Require controllers.
@@ -11,5 +12,35 @@ router.get('/', welcomeController.index);
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+
+
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
