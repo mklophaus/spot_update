4565a3fbaa517e390045d7f0f8685daf7a5c6be8
Mike Klophaus <mmklophaus@gmail.com>
2016-01-20 11:25:37 -0800

    made changes to user Schema (updateSchema added)

--- models/user.js
+++ models/user.js
@@ -1,9 +1,17 @@
 var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
+var updateSchema = new mongoose.Schema({
+  text: String,
+  created: {type: Date, default: Date.now}
+});
+
 var userSchema = new mongoose.Schema({
   name:   String,
-  handle: String
+  email: String,
+  googleId: String,
+  updates: [updateSchema],
+  created: {type: Date, default: Date.now}
 });
 
 var User = mongoose.model('User', userSchema);
