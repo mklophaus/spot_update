0e4e1d7fd0aed14cddadca15c277c66af3faf0b5
Mike Klophaus <mmklophaus@gmail.com>
2016-01-20 11:26:45 -0800

    started passport implementation

---
+++ config/passport.js
@@ -0,0 +1,45 @@
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var User = require('../models/user.js');
+
+module.exports = function(passport){
+
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+
+  function(accessToken, refreshToken, profile, done) {
+    User.findOne({ 'googleId': profile.id }, function(err, student) {
+      if (err) return done(err);
+      //(null) refers to no error...
+      if (student) { return done(null, student);
+     } else {
+   // we have a new student via OAuth!
+         var newUser = new User({
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          googleId: profile.id
+         });
+
+         newUser.save(function(err) {
+
+          if (err) return done(err);
+          return done(null, newUser);
+        });
+      }
+    });
+  }
+));
+
+  passport.serializeUser(function(user, done) {
+    done(null, student.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+};
