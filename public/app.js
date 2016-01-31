96b000e12b119749fd7ba9a29c5e20de0243eb10
Mike Klophaus <mmklophaus@gmail.com>
2016-01-31 12:11:48 -0500

    angular app file added

---
+++ public/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+  angular
+    .module("spotApp", [
+      "ui.router",
+      "uiGmapgoogle-maps"    ])
+
+    // .config(function($httpProvider) {
+    //   $httpProvider.interceptors.push('authInterceptor');
+    // });
+
+
+})();
